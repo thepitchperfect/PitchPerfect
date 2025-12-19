314c9f18b1f99ec8f3eef6ef6996360878124ed1
RafaMiyauchi <rafamiyauchi@gmail.com>
2025-12-19 12:20:51 +0700

    Updating stuff for Flutter Support

--- club_directories/urls.py
+++ club_directories/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_club_directory, get_club_details, set_league_pick 
+from .views import show_club_directory, get_club_details, set_league_pick ,show_json_directory
 
 app_name = 'club_directories'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', show_club_directory, name='show_club_directory'),
     path('club/<uuid:club_id>/', get_club_details, name='get_club_details'),
     path('set-league-pick/', set_league_pick, name='set_league_pick'),
+    path('api/', show_json_directory, name='show_json_directory'),
 ]
